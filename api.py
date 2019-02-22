07f4899233b793b16f573cd9c05abb1d5f0add8a
GuyMcFail <34040408+GuyMcFail@users.noreply.github.com>
2019-02-21 21:50:14 -0500

    test

--- api.py
+++ api.py
@@ -1,12 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
+import io
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./secrets/ccpk.json"
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    './images/SkullD.png')
+
+# Loads the image into memory
+with io.open(file_name, 'rb') as image_file:
+    content = image_file.read()
+
+image = types.Image(content=content)
+
+# Performs label detection on the image file
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/index.html")
 def hello():
-    return "Hello World!"
-
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run()
