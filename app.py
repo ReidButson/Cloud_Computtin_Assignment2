14d7642b2ac6b2cd4ee11f23033f6d4384e99819
GuyMcFail <34040408+GuyMcFail@users.noreply.github.com>
2019-02-22 00:09:23 -0500

    minor fix

--- app.py
+++ app.py
@@ -3,33 +3,12 @@ from pathlib import Path
 import io
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./secrets/ccpk.json"
-'''
+
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    './static/SkullD.png')
-
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
-
-image = types.Image(content=content)
-
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-print('Labels:')
-for label in labels:
-    print(label.description)
-'''
 app = Flask(__name__)
 
 skull = "./static/SkullD.png"
@@ -48,8 +27,28 @@ def upload_image():
     destination = Path.cwd().joinpath("static", "uploads", filename)
     file.save(str(destination))
 
-    #return "<img src='./static/uploads/{}' alt=\"y tho\">".format(filename)
-    return render_template("itworked.html", imsrc="/static/uploads/{}".format(filename))
+
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+
+    # Loads the image into memory
+    with io.open(str(destination), 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+    relative_path = "/static/uploads/{}".format(filename)
+    return render_template("itworked.html", imsrc=relative_path)
 
 if __name__ == "__main__":
     app.run()
